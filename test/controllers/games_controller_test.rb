20fffa47f98ee2df177d0789b52c6ed436523053
Xenoveritas <dmpotter@xenoveritas.org>
2017-09-29 00:58:18 -0400

    Add tests for the games controller

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @game = games(:game_one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:games)
+  end
+
+  test "should create game" do
+    assert_difference('Game.count') do
+      post :new, barcode: 'barcode2', title: 'New Game', publisher: 'New Publisher'
+    end
+
+    assert_response :success
+  end
+
+  test "should show game" do
+    xhr :get, :display, barcode: @game.barcode
+    assert_response :success
+  end
+
 end
